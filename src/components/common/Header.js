5f12fb4204df045c87352c370dbc72072d30ce69
harshit5 <harshit@xejic.com>
2019-01-02 16:33:05 +0530

    Updated Header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,18 +4,17 @@ import { Link } from 'react-router-dom';
 import logo from './logo.png';
 import Search from './Search';
 
-class Header extends Component {
-    render() {
-      return (
-        <div className="Header">
-          <Link to = "/" > 
-            <img src={logo} alt="logo" className="Header-logo" />
-          </Link>
+const Header = () => {
+  return (
+    <div className="Header">
+      <Link to="/">
+        <img src={logo} alt="logo" className="Header-logo" />
+      </Link>
 
-          <Search />
-        </div>
-      );
-    }
-  }
-  
-  export default Header;
+      <Search />
+    </div>
+  );
+}
+
+export default Header;
+
